02a748bc6ee1f2d5e039543324188d537de3b434
pedrodemiangodinezcruz <30833341+pedrodemiangodinezcruz@users.noreply.github.com>
2022-03-09 14:09:39 -0600

    Anadicion de mas secciones del instructivo

--- public/js/mostrar-tablas.js
+++ public/js/mostrar-tablas.js
@@ -6,6 +6,37 @@
       document.getElementById("tabla2").onclick = function() {
       document.getElementById("2").style.display = "none";
       document.getElementById("1").style.display = "table";
-
       }
+      document.getElementById("tabla3").onclick = function() {
+        document.getElementById("2").style.display = "none";
+        document.getElementById("3").style.display = "table";
+    }
+    document.getElementById("regresar-tabla2").onclick = function() {
+        document.getElementById("3").style.display = "none";
+        document.getElementById("2").style.display = "table";
+    }
+    document.getElementById("ir-tabla-4").onclick = function() {
+        document.getElementById("3").style.display = "none";
+        document.getElementById("4").style.display = "table";
+    }
+    document.getElementById("regresar-tabla3").onclick = function() {
+        document.getElementById("4").style.display = "none";
+        document.getElementById("3").style.display = "table";
+    }
+    document.getElementById("ir-tabla-5").onclick = function() {
+        document.getElementById("4").style.display = "none";
+        document.getElementById("5").style.display = "table";
+    }
+    document.getElementById("regresar-tabla4").onclick = function() {
+        document.getElementById("5").style.display = "none";
+        document.getElementById("4").style.display = "table";
+    }
+    document.getElementById("ir-tabla-6").onclick = function() {
+        document.getElementById("5").style.display = "none";
+        document.getElementById("6").style.display = "table";
+    }
+    document.getElementById("regresar-tabla5").onclick = function() {
+        document.getElementById("6").style.display = "none";
+        document.getElementById("5").style.display = "table";
+    }
 
