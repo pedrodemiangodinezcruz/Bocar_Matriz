59f838b00c6db1519eee0556a98d4b6d37f21f6d
pedrodemiangodinezcruz <30833341+pedrodemiangodinezcruz@users.noreply.github.com>
2022-03-09 09:56:43 -0600

    Funcionalidad pasar sobre tablas

---
+++ public/js/mostrar-tablas.js
@@ -0,0 +1,11 @@
+
+      document.getElementById("tabla1").onclick = function() {
+        document.getElementById("1").style.display = "none";
+        document.getElementById("2").style.display = "table";
+      }
+      document.getElementById("tabla2").onclick = function() {
+      document.getElementById("2").style.display = "none";
+      document.getElementById("1").style.display = "table";
+
+      }
+
