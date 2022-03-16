cef432ae5e13ba3891632b24adb81d86a38e0a75
pedrodemiangodinezcruz <30833341+pedrodemiangodinezcruz@users.noreply.github.com>
2022-03-16 12:40:01 -0600

    Estilos v1.0

--- public/js/mostrar-tablas.js
+++ public/js/mostrar-tablas.js
@@ -71,4 +71,39 @@
         document.getElementById("10").style.display = "none";
         document.getElementById("9").style.display = "table";
     }
+
+    document.getElementById("ir-tabla-11").onclick = function() {
+        document.getElementById("10").style.display = "none";
+        document.getElementById("11").style.display = "table";
+    }
+    document.getElementById("regresar-tabla10").onclick = function() {
+        //document.getElementById("12").style.display = "none";
+        document.getElementById("11").style.display = "none";
+        document.getElementById("10").style.display = "table";
+    }
+    document.getElementById("ir-tabla-12").onclick = function() {
+        document.getElementById("11").style.display = "none";
+        document.getElementById("12").style.display = "table";
+    }
+    document.getElementById("regresar-tablita").onclick = function() {
+        document.getElementById("12").style.display = "none";
+        document.getElementById("11").style.display = "table";
+    }
+    document.getElementById("ir-tabla-13").onclick = function() {
+        document.getElementById("11").style.display = "none";
+        document.getElementById("13").style.display = "table";
+    }
+    document.getElementById("regresar-tabla11").onclick = function() {
+        document.getElementById("13").style.display = "none";
+        document.getElementById("11").style.display = "table";
+    }
+    document.getElementById("ir-tabla-14").onclick = function() {
+        document.getElementById("13").style.display = "none";
+        document.getElementById("14").style.display = "table";
+    }
+    document.getElementById("regresar-tabla13").onclick = function() {
+        document.getElementById("14").style.display = "none";
+        document.getElementById("13").style.display = "table";
+    }
+    
     
