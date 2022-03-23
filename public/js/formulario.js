5143d5c41a5ce8be61291765594c70df0a4e6ca1
pedrodemiangodinezcruz <30833341+pedrodemiangodinezcruz@users.noreply.github.com>
2022-03-23 14:27:07 -0600

    Formulario completado
    Incluida funcionalidad de boton con script de mas o menos

---
+++ public/js/formulario.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+    var quantitiy=0;
+       $('.quantity-right-plus').click(function(e){
+            
+            // Deja de funcionar como un boton
+            e.preventDefault();
+            // Toma el id del campo
+            var quantity = parseInt($('#quantity').val());
+            
+            // Incrementar
+                $('#quantity').val(quantity + 1);
+            
+        });
+    
+         $('.quantity-left-minus').click(function(e){
+            
+            e.preventDefault();
+        
+            var quantity = parseInt($('#quantity').val());
+            
+        
+          
+                // Disminuir
+                if(quantity>0){
+                $('#quantity').val(quantity - 1);
+                }
+        });
+    });
