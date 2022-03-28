75e17da263aa453f2f8e147767af66495756ed59
pedrodemiangodinezcruz <30833341+pedrodemiangodinezcruz@users.noreply.github.com>
2022-03-28 10:41:20 -0600

    Diseño Gráficos faltantes
    Diseño del último gráfico de pastel y Pareto

--- public/js/mostrarEstadisticas.js
+++ public/js/mostrarEstadisticas.js
@@ -5,5 +5,14 @@ document.getElementById("ir-tabla-controles").onclick = function() {
 }
 document.getElementById("regresar-tabla-riesgoInherente").onclick = function() {
     document.getElementById("controles").style.display = "none";
-    document.getElementById("riesgoInherente").style.display = "block";
+    document.getElementById("riesgoInherente").style.display = "initial";
+}
+
+document.getElementById("ir-tabla-riesgoResidual").onclick = function() {
+    document.getElementById("controles").style.display = "none";
+    document.getElementById("riesgoResidual").style.display = "initial";
+}
+document.getElementById("regresar-tabla-controles").onclick = function() {
+    document.getElementById("riesgoResidual").style.display = "none";
+    document.getElementById("controles").style.display = "initial";
 }
