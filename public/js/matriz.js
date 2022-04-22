4e265ef75f00c6fb5271e094a693bd5eb71e3935
pedrodemiangodinezcruz <30833341+pedrodemiangodinezcruz@users.noreply.github.com>
2022-04-22 12:28:28 -0500

    Content editable

--- public/js/matriz.js
+++ public/js/matriz.js
@@ -1,6 +1,7 @@
 //Dependiendo del id(sección) de las tablas, hacer visible u ocultar las secciones
-document.getElementById("botonEditar").onclick = function() {
-    document.getElementById("botonEditar").style.display = "none";
-    document.getElementById("botonGuardar").style.display = "block";
-    console.log("Funciona");
-  }
+	document.getElementById("botonEditar").onclick = function() {
+		document.getElementById("botonEditar").style.display = "none";
+		document.getElementById("botonGuardar").style.display = "block";
+		
+	  }
+	  
