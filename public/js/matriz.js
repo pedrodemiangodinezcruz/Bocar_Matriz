1274b36d9786c735ab4c7ea31aab2a13ff954cb9
pedrodemiangodinezcruz <30833341+pedrodemiangodinezcruz@users.noreply.github.com>
2022-04-22 11:01:44 -0500

    Cambios bvotones

---
+++ public/js/matriz.js
@@ -0,0 +1,6 @@
+//Dependiendo del id(sección) de las tablas, hacer visible u ocultar las secciones
+document.getElementById("botonEditar").onclick = function() {
+    document.getElementById("botonEditar").style.display = "none";
+    document.getElementById("botonGuardar").style.display = "block";
+    console.log("Funciona");
+  }
