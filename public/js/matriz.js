8357e5881e4b4cfe4421ca3232d25c79e0d9213e
pedrodemiangodinezcruz <30833341+pedrodemiangodinezcruz@users.noreply.github.com>
2022-04-25 09:46:03 -0500

    Boton edición de riesgos terminado

--- public/js/matriz.js
+++ public/js/matriz.js
@@ -3,11 +3,9 @@
 		document.getElementById("botonEditar").style.display = "none";
 		document.getElementById("botonGuardar").style.display = "block";
 		const tabla = document.getElementsByClassName("editable");
-		for (var i=0; i < tabla.length; i++) {
-		var editable = document.getElementById(editable);
-		editable.contentEditable = true;
-		document.getElementById("editable"+i).contentEditable = true;
-		console.log(i);
-		$("img").attr("width","500");
-	  }
+		//for (var i=0; i < tabla.length; i++) {
+			/*Cambiar el atributo global contentEditable a verdadero para poder editar los valores
+			de los riesgos y controles*/
+		$(".editable").attr("contentEditable", true);
+	  //}
 	}
